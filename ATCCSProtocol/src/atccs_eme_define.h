9a16eba906dd626e983fa48df5431a80abe8c060
iamgeniuswei <iamgeniuswei@sina.com>
2017-06-23 17:15:20 +0800

    version 3.00.00 1) modify ENUM_AT_MSG_TYPE 2) modify atccs_eme_define.h, remove the useless fields in data struct.

--- ATCCSProtocol/src/atccs_eme_define.h
+++ ATCCSProtocol/src/atccs_eme_define.h
@@ -30,85 +30,114 @@
  * desciption:          use ATCCS_GLOBAL_H to redefine the data type
  */
 #pragma once
+#include <math.h>
+
 #include "atccs_global.h"
 
-//data structure of ASC.
-//sizeof(ASC_STRUCT) = 24bytes.
+/**
+ * ASC realtime data structure.
+ * The programmer have to guarantee that:
+ * sizeof(double) == 8
+ * sizeof(unsigned short) == 2 
+ * sizeof(_EME_ASC_REALTIME_DATA) == 24
+ */
 #pragma pack(1)
-typedef struct asc_struct{
-    UINT32 envDeviceType;
-    UINT32 timestamp;
-    FLOAT32 shutterspeed;
-    FLOAT32 aperture;
+struct _EME_ASC_REALTIME_DATA
+{
+    double shutterspeed;
+    double aperture;
     unsigned short iso;
     unsigned short jpeg;
     unsigned short raw;
     unsigned short fits;
-}_AT_ASC_STATUS;
+};
 #pragma pack()
 
-//data structure of WS.
-//sizeof(WS_STRUCT) = 52bytes;
+/**
+ * WS realtime data structure.
+ * The programmer have to guarantee that:
+ * sizeof(double) == 8
+ * sizeof(unsigned int) == 4 
+ * sizeof(_EME_WS_REALTIME_DATA) == 84
+ */
 #pragma pack(1)
-typedef struct ws_struct{
-    UINT32 envDeviceType;
-    UINT32 timestamp;
-    FLOAT32 temperature;
-    FLOAT32 humidity;
-    FLOAT32 dewpoint;
-    FLOAT32 pressure;
-    FLOAT32 windspeed;
-    FLOAT32 windspeed2;
-    FLOAT32 windspeed10;
-    UINT32 windderection;
-    FLOAT32 rainfall;
-    FLOAT32 raidation;
-    FLOAT32 illumination;
-}_AT_WS_STATUS;
+struct _EME_WS_REALTIME_DATA
+{
+    double temperature;
+    double humidity;
+    double dewpoint;
+    double pressure;
+    double windspeed;
+    double windspeed2;
+    double windspeed10;
+    unsigned int windderection;
+    double rainfall;
+    double raidation;
+    double illumination;
+};
 #pragma pack()
-//data structure of CS.
-//sizeof(CS_STRUCT) = 42bytes.
+
+/**
+ * WS realtime data structure.
+ * The programmer have to guarantee that:
+ * sizeof(double) == 8
+ * sizeof(unsigned short) == 2 
+ * sizeof(_EME_WS_REALTIME_DATA) == 62
+ */
 #pragma pack(1)
-typedef struct cs_struct{
-    UINT32 envDeviceType;
-    UINT32 timestamp;
-    FLOAT32 RelSkyT;
-    FLOAT32 AmbientT;
-    FLOAT32 SensorT;
+struct _EME_CS_REALTIME_DATA
+{
+    double RelSkyT;
+    double AmbientT;
+    double SensorT;
     unsigned short RainF;
     unsigned short RainThreshV;
-    FLOAT32 wind;
+    double wind;
     unsigned short WindUnits;
-    FLOAT32 HumidityPercent;
-    FLOAT32 DewPointT;
-    UINT32 DayLightV;
-}_AT_CS_STATUS;
+    double HumidityPercent;
+    double DewPointT;
+    double DayLightV;
+};
 #pragma pack()
-//data structure of SQM.
-//sizeof(SQM_STRUCT) = 12bytes;
+
+
+/**
+ * WS realtime data structure.
+ * The programmer have to guarantee that:
+ * sizeof(double) == 8
+ * sizeof(_EME_SQM_REALTIME_DATA) == 8
+ */
 #pragma pack(1)
-typedef struct sqm_struct{
-    UINT32 envDeviceType;
-    UINT32 timestamp;
-    FLOAT32 sqmdata;
-}_AT_SQM_STATUS;
+struct _EME_SQM_REALTIME_DATA
+{
+    double sqmdata;
+};
 #pragma pack()
-//data structure of dust.
-//sizeof(DUST_STRUCT) = 12bytes;
+
+
+/**
+ * WS realtime data structure.
+ * The programmer have to guarantee that:
+ * sizeof(double) == 8
+ * sizeof(_EME_DUST_REALTIME_DATA) == 8
+ */
 #pragma pack(1)
-typedef struct dust_struct{
-    UINT32 envDeviceType;
-    UINT32 timestamp;
-    FLOAT32 dust_data;
-}_AT_DUST_STATUS;
+struct _EME_DUST_REALTIME_DATA
+{
+    double dust_data;
+};
 #pragma pack()
-//data structure of dimm.
-//sizeof(DUST_STRUCT) = 12bytes;
+
+/**
+ * WS realtime data structure.
+ * The programmer have to guarantee that:
+ * sizeof(double) == 8
+ * sizeof(_EME_DIMM_REALTIME_DATA) == 8
+ */
 #pragma pack(1)
-typedef struct dimm_struct{
-    UINT32 envDeviceType;
-    UINT32 timestamp;
-    FLOAT32 dimm_data;
-}_AT_DIMM_STATUS;
+struct _EME_DIMM_REALTIME_DATA
+{
+    double dimm_data;
+};
 #pragma pack()
 #endif // ATCCS_EME_DEFINE_H
